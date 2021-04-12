8f19f29c128b0a82a1c6d826b359ec779727a4da
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-04-12 12:35:22 +0700

    commit
    update backend

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Cơm Sườn',
+      category: 'Cơm',
+      image: '/images/d1.jpg',
+      price: 50000,
+      brand: 'Cô 3',
+      rating: 4.5,
+      numReviews: 10,
+    },
+
+    {
+      _id: '2',
+      name: 'Cơm Sườn',
+      category: 'Cơm',
+      image: '/images/d1.jpg',
+      price: 40000,
+      brand: 'Cô 3',
+      rating: 4.2,
+      numReviews: 8,
+    },
+
+    {
+      _id: '3',
+      name: 'Phở Bò',
+      category: 'Phở',
+      image: '/images/d1.jpg',
+      price: 55000,
+      brand: 'Cô 3',
+      rating: 4.0,
+      numReviews: 6,
+    },
+
+    {
+      _id: '4',
+      name: 'Phở Gà',
+      category: 'Phở',
+      image: '/images/d1.jpg',
+      price: 30000,
+      brand: 'Cô 3',
+      rating: 3.5,
+      numReviews: 10,
+    },
+
+  ],
+};
